f0d75c6d4a7772e41b391d60ce350d472c2fc3b6
Charlotte Delvaux <stargrasske@gmail.com>
2025-06-10 10:36:38 +0200

    Update files for v2 deployment

--- word-picker/word_picker.py
+++ word-picker/word_picker.py
@@ -1,10 +1,17 @@
+from os import environ
 from flask import Flask
 from random import choice
 
 app = Flask(__name__)
+options_file = environ.get("OPTIONS_FILE", "options.txt")
 
-choices = ["first", "second", "third"]
+def load_options(file_path):
+    with open(file_path, 'r', encoding = "utf-8") as file:
+        file_contents = file.read()
+    return file_contents.split()
+
+options = load_options(options_file)
 
 @app.route('/')
 def pick_word():
-    return choice(choices)
+    return choice(options)
